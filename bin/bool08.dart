f3965c6120ff88f7215698f69b00058bfe08a509
timur <ttoshte@gmail.com>
2022-09-30 17:08:47 +0300

    8

--- bin/bool08.dart
+++ bin/bool08.dart
@@ -1,10 +1,15 @@
 /*
-    Create function func
+    lCreate function func
     check the whole number. Integers are 0 and a positive number.
     Args:
         a: int
     Returns:
-        bool
+        boo
     */
 
-void main() {}
+void main() {
+print(func(7));    
+}
+bool func(int a ){
+return a % 1 == 0 && 7 >=0;    
+}
