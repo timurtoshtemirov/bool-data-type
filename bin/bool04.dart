8ce21b90e3c3d6c811597334062d39ef6730e99d
timur <ttoshte@gmail.com>
2022-10-04 12:35:43 +0300

    4

--- bin/bool04.dart
+++ bin/bool04.dart
@@ -1,16 +1,6 @@
-/*
-    Create funcrtion func
-    Check the logic "The variable "a" is negative"
-    Args:
-        a: int
-    Returns:
-        bool
-    */
-
-void main() {
- bool x;
- x = true;
- x = false;
- int  r =7;
- print(r > 0);   
+void main(){
+ print(func(-2));   
+}
+bool func (int b){
+ return b > 2;   
 }
